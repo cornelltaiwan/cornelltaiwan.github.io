0fc7881c6e5569e805c49324a189f688ae190baa
Albert <ak722@cornell.edu>
2016-07-25 20:25:43 -0400

    fix mail v6

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://localhost/mail/mail.php",
+                url: "././mail/mail.php",
                 type: "POST",
                 contentType: "application/json; charset=utf-8",
                 data: {
