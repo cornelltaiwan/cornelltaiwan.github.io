7251fb21f06899535e86dc7a6277a55fd1421fb3
Albert <ak722@cornell.edu>
2016-07-25 20:13:02 -0400

    fix mail v2

--- mail/mail.php
+++ mail/mail.php
@@ -1,4 +1,8 @@
 <?php
+    // maybe this will help
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: *");
+
     // Build email
     $recipient = "ak722@cornell.edu";
     $subject = "test from $name";
