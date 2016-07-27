daae89d8a47f6727646b2ec575bdcca75b12979b
Albert Kung <albertkung43@gmail.com>
2016-07-27 17:41:14 -0400

    insta fix v3

--- js/insta.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    var feed = new Instafeed({
-        get: 'user',
-        clientId: '319408ee2edb42519b76507debbd3b0c',
-        userID: '2153603227'
-        target: 'instagram'
-    });
-    feed.run();
-</script>
